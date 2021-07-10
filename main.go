d6ea4c90a93c344ea0e7faa4211c24111bf3c170
lottotto <kentaro.saito50@gmail.com>
2021-07-10 22:21:36 +0900

    #7 ミスがあったので修正

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"github.com/lottotto/todo-app/db"
 	"github.com/lottotto/todo-app/router"
 )
 
 func main() {
-	e := router.Init(db.Init())
+	e := router.Init()
 	e.Logger.Fatal(e.Start(":1323"))
 }
