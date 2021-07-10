d4bc817c81c9a8c83dd197e8dda01d5104be9858
lottotto <kentaro.saito50@gmail.com>
2021-07-10 17:47:52 +0900

    add test code:

--- router/route.go
+++ router/route.go
@@ -1,23 +1,24 @@
 package router
 
 import (
+	"database/sql"
+
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/lottotto/todo-app/api"
-	"github.com/lottotto/todo-app/db"
 )
 
-func Init() *echo.Echo {
-	return getRouter()
+func Init(db *sql.DB) *echo.Echo {
+	return getRouter(db)
 }
-func getRouter() *echo.Echo {
+func getRouter(db *sql.DB) *echo.Echo {
 	e := echo.New()
 
 	//middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	handler := api.Handler{DB: db.Init()}
+	handler := api.Handler{DB: db}
 
 	e.GET("/hello", handler.GetHello)
 	e.GET("/status", handler.GetHealthCheck)
